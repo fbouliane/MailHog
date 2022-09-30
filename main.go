6c7500effa176f75f6bf24c5ec432de20fbeebf3
Félix Bouliane <felixbouliane@gmail.com>
2022-09-30 16:20:01 -0400

    use my MailHog-UI fork

--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import (
 
 	gohttp "net/http"
 
+	"github.com/fbouliane/MailHog-UI/assets"
+	cfgui "github.com/fbouliane/MailHog-UI/config"
+	cfgcom "github.com/fbouliane/MailHog/config"
 	"github.com/gorilla/pat"
 	"github.com/ian-kent/go-log/log"
 	"github.com/mailhog/MailHog-Server/api"
 	cfgapi "github.com/mailhog/MailHog-Server/config"
 	"github.com/mailhog/MailHog-Server/smtp"
-	"github.com/mailhog/MailHog-UI/assets"
-	cfgui "github.com/mailhog/MailHog-UI/config"
 	"github.com/mailhog/MailHog-UI/web"
-	cfgcom "github.com/mailhog/MailHog/config"
 	"github.com/mailhog/http"
 	"github.com/mailhog/mhsendmail/cmd"
 	"golang.org/x/crypto/bcrypt"
